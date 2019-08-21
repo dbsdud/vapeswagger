d680cf6dd225efb9f9c29b877ebf305c730a9461
duddl425 <duddl425@gmail.com>
2019-08-21 19:46:48 +0900

    extraWin 수정

--- frontend/src/components/notice/win/extraWin.jsx
+++ frontend/src/components/notice/win/extraWin.jsx
@@ -22,25 +22,8 @@ export default function SimpleMenu(thisIbProp) {
   function handleClose() {
     setAnchorEl(null);
   }
-  if(userNickName === thisIbProp.ntDetailResponse.noticeWriter) {  
-    return (  
-      <div>
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-          <img src={ extra } className="extraBtn" />
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}><Link to={`/noticeModify/${ thisIbProp.ntDetailResponse.noticeNo }`}><i className="fa fa-edit"></i> 수정</Link></MenuItem>
-          <MenuItem onClick={noticeDel}><i className="fa fa-trash"></i> 삭제</MenuItem>
-        </Menu>
-      </div>
-    );
-  } else if(userNickName === 'temp') {
+  // 1. 로그인이 안되어 있을 때
+  if(userNickName === 'temp') {  
     return (
       <div>
         <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
@@ -57,23 +40,44 @@ export default function SimpleMenu(thisIbProp) {
         </Menu>
       </div>    
     );
-  } else if(userNickName !== thisIbProp.ntDetailResponse.noticeWriter) {
-    return (
-      <div>
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-          <img src={ extra } className="extraBtn" />
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={noticeReport}><i className="fa fa-exclamation-circle"></i> 신고</MenuItem>
-        </Menu>
-      </div>    
-    );
-  } 
+  } else { // 2. 로그인이 되어 있을 때
+    // 2-1. 로그인된 계정과 작성자가 같을 경우
+    if(userNickName === thisIbProp.ntDetailResponse.noticeWriter) {
+      return (  
+        <div>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <img src={ extra } className="extraBtn" />
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}><Link to={`/noticeModify/${ thisIbProp.ntDetailResponse.noticeNo }`}><i className="fa fa-edit"></i> 수정</Link></MenuItem>
+            <MenuItem onClick={noticeDel}><i className="fa fa-trash"></i> 삭제</MenuItem>
+          </Menu>
+        </div>
+      );
+    } else { // 2-2. 로그인된 계정과 작성자가 같지 않을 경우
+      return (
+        <div>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <img src={ extra } className="extraBtn" />
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={noticeReport}><i className="fa fa-exclamation-circle"></i> 신고</MenuItem>
+          </Menu>
+        </div>    
+      )
+    }
+  }
 }
 
